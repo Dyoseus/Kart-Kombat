d68be8b7be68020b9f1f276e413b23e0fe23e8a7
Albert Le <albert_h_le@hotmail.com>
2023-06-06 15:25:47 -0400

    mines work now from getting crate

--- GoKart Runaway/Assets/Scripts/PowerUpCrate.cs
+++ GoKart Runaway/Assets/Scripts/PowerUpCrate.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 public class PowerUpCrate : MonoBehaviour
 {
     public GameObject fireBallPrefab;
+    public GameObject mine;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<FireFireBall>().GiveFireBallAbility(fireBallPrefab);
+            other.GetComponent<dropLandmines>().hasMines = true;
             Destroy(gameObject);
         }
     }
